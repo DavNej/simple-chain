d25e48146eaf40a265959f4c7e18e86e873550a3
DavNej <david.nejar@gmail.com>
2024-01-28 20:13:36 +0100

    setup testing-library/react

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,15 @@
-import path from 'path'
+/// <reference types="vitest" />
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react-swc'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
-// https://vitejs.dev/config/
+// https://vitejs.dev/config
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
+  plugins: [react(), tsconfigPaths()],
+  test: {
+    globals: true,
+    environment: 'happy-dom',
+    setupFiles: 'tests/setup',
+    include: ['tests/**/*.test.{ts,tsx}'],
   },
 })
