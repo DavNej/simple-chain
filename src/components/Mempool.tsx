4e8a4cdadabd8d7b7bd9abe2932b8d08d6880f1e
DavNej <david.nejar@gmail.com>
2024-02-12 17:21:40 +0100

    add Mempool component

---
+++ src/components/Mempool.tsx
@@ -0,0 +1,40 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card'
+import Transaction from '@/lib/chain/transaction'
+import TransactionCard from './TransactionCard'
+import { ScrollArea } from '@/components/ui/scroll-area'
+
+export default function Mempool({
+  transactions,
+}: {
+  transactions: Transaction[]
+}) {
+  return (
+    <Card className="flex flex-col">
+      <CardHeader>
+        <CardTitle>Mempool</CardTitle>
+        <CardDescription>
+          The memory pool is a holding area for transactions that have been
+          broadcast to the network but not yet included in a block. We can see
+          it as a waiting room where transactions stay until they are picked up
+          by &ldquo;block builders&rdquo; and added to a new block, making them
+          part of the blockchain. The mempool is where transactions are waiting
+          to be confirmed and written into a block
+        </CardDescription>
+      </CardHeader>
+
+      <CardContent className="flex-1">
+        <ScrollArea className="flex  h-[200px] flex-col gap-2">
+          {transactions.map(tx => (
+            <TransactionCard key={tx.hash} tx={tx} />
+          ))}
+        </ScrollArea>
+      </CardContent>
+    </Card>
+  )
+}
---
+++ tests/components/Mempool.test.tsx
@@ -0,0 +1,20 @@
+import { render, screen } from '@testing-library/react'
+import Mempool from '@/components/Mempool'
+import { buildTransactionBatch } from 'tests/test-utils/helpers'
+
+describe('Mempool', () => {
+  it('renders without crashing', () => {
+    const transactions = buildTransactionBatch()
+
+    render(<Mempool transactions={transactions} />)
+
+    expect(screen.getByText('Mempool')).toBeInTheDocument()
+    expect(
+      screen.getByText(/The memory pool is a holding area for transactions/),
+    ).toBeInTheDocument()
+
+    transactions.forEach(tx => {
+      expect(screen.getByText(tx.hash)).toBeInTheDocument()
+    })
+  })
+})
