2975de5e928c2feb8776ea06e7dc2e600e943936
DavNej <david.nejar@gmail.com>
2024-02-22 18:10:00 +0100

    add generateMnemonicPhrase utils

--- src/lib/crypto.ts
+++ src/lib/crypto.ts
@@ -4,3 +4,8 @@ export function generateAddress(): string {
   const wallet = ethers.Wallet.createRandom()
   return wallet.deriveChild(Math.floor(Math.random() * 100)).address
 }
+
+export function generateMnemonicPhrase(): string {
+  const wallet = ethers.Wallet.createRandom()
+  return wallet.mnemonic!.phrase
+}
